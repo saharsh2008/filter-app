db774f37a905e00fbf13d8b2846b8a057a93a5ed
saharsh2008 <69161165+saharsh2008@users.noreply.github.com>
2021-04-23 15:02:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+MustacheX = 0;
+MustacheY = 0;
+
+function preload() {
+    Mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses)
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(Mustache, MustacheX, MustacheY, 60, 30);
+}
+
+function snapShot() {
+    save("picture.png");
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        MustacheX = results[0].pose.nose.x - 30;
+        MustacheY = results[0].pose.nose.y - 5;
+        console.log(`Mustache X = ${MustacheX}`);
+        console.log(`Mustache Y = ${MustacheY}`);
+    }
+}
